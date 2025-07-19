e6733f6033ccca0b210aca6ea9ccbece21c94018
Leonardo Oliveira <leonardo.os@outlook.com>
2025-07-19 16:36:33 -0300

    atualização de logica2

--- logica2.js
+++ logica2.js
@@ -1,6 +1,17 @@
-let nome = "Brasa "
-let frase1 = "O herói de nome "
-let xp = "ouro"
-let frase2 = "está no nível de "
+function main(vitorias, derrotas) {
+    const saldo = vitorias - derrotas;
+    let nivel = "";
 
-console.log(frase1 + nome + frase2 + xp)
+    if (vitorias < 10) {nivel = "ferro";}
+    else if (vitorias => 20) {nivel = "bronze"}
+    else if (vitorias => 50) {nivel = "prata"}
+    else if (vitorias => 80) {nivel = "ouro"}
+    else if (vitorias => 90) {nivel = "diamente"}
+    else if (vitorias => 100) {nivel = "lendario"}
+    else if (vitorios => 102) {nivel = "imertoal"}
+
+    return "O heroi tem saldo de " + saldo + " vitórias e está no nível " + nivel 
+}
+
+console.log(main(55, 20))
+
